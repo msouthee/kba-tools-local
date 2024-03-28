dcb2a96299c9f9842294d27571a5396f7127b143
msouthee <megsouthee@gmail.com>
2024-03-28 12:01:04 -0400

    Remove SymbologyError exception

--- KBAToolsLocal/KBAExceptions.py
+++ KBAToolsLocal/KBAExceptions.py
@@ -24,8 +24,3 @@ class SpeciesDataError(Exception):
 class BioticsError(Exception):
     """Exception raised for BioticsError in the tool."""
     pass
-
-
-class SymbologyError(Exception):
-    """Exception raised for SymbologyError in the tool."""
-    pass
